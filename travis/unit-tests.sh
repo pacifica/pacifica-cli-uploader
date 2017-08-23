f85b12ce30feea3c259e6a6159b8274f51585d43
dmlb2000 <dmlb2000@gmail.com>
2017-08-23 10:55:41 -0700

    get the unit test to run

--- travis/unit-tests.sh
+++ travis/unit-tests.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 export POSTGRES_ENV_POSTGRES_USER=postgres
 export POSTGRES_ENV_POSTGRES_PASSWORD=
+export UPLOADER_CONFIG=travis/uploader.json
 coverage run --include='uploader_cli/*,CLIUploader.py' -p CLIUploader.py --help
 coverage report --show-missing --fail-under 100
 if [[ $CODECLIMATE_REPO_TOKEN ]] ; then
