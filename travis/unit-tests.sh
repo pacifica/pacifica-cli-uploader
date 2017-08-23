fd042bb7cdf2cd96b10434c3b7ab2bd6852cc217
dmlb2000 <dmlb2000@gmail.com>
2017-08-23 08:46:26 -0700

    get the right coverage as well

--- travis/unit-tests.sh
+++ travis/unit-tests.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export POSTGRES_ENV_POSTGRES_USER=postgres
 export POSTGRES_ENV_POSTGRES_PASSWORD=
-coverage run --include='cli/*,CLIUploader.py' -p CLIUploader.py --help
+coverage run --include='pacifica/uploader/cli/*,CLIUploader.py' -p CLIUploader.py --help
 coverage report --show-missing --fail-under 100
 if [[ $CODECLIMATE_REPO_TOKEN ]] ; then
   codeclimate-test-reporter
