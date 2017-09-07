2d40cbb3d1bccba84f36a905c0840934fd460f6e
David Brown <dmlb2000@gmail.com>
2017-09-07 10:22:56 -0700

    organize some of the tests

--- travis/unit-tests.sh
+++ travis/unit-tests.sh
@@ -1,14 +1,30 @@
 #!/bin/bash
+############################
+# Help commands
+############################
 coverage run --include='uploader_cli/*,CLIUploader.py' CLIUploader.py --config travis/uploader.json --help
 coverage run --include='uploader_cli/*,CLIUploader.py' -a CLIUploader.py --config=travis/uploader.json --help
 export UPLOADER_CONFIG=travis/uploader.json
 coverage run --include='uploader_cli/*,CLIUploader.py' -a CLIUploader.py query --help
-coverage run --include='uploader_cli/*,CLIUploader.py' -a CLIUploader.py query -q instrument regex
 coverage run --include='uploader_cli/*,CLIUploader.py' -a CLIUploader.py upload --help
-coverage run --include='uploader_cli/*,CLIUploader.py' -a CLIUploader.py upload --logon 10 --instrument 54 --proposal 1234a --user-of-record 11 file
 coverage run --include='uploader_cli/*,CLIUploader.py' -a CLIUploader.py configure --help
+
+############################
+# Query commands
+############################
+coverage run --include='uploader_cli/*,CLIUploader.py' -a CLIUploader.py query -q instrument regex
+
+############################
+# Upload commands
+############################
+coverage run --include='uploader_cli/*,CLIUploader.py' -a CLIUploader.py upload --logon 10 --instrument 54 --proposal 1234a --user-of-record 11 file
+
+############################
+# Configure commands
+############################
 yes | coverage run --include='uploader_cli/*,CLIUploader.py' -a CLIUploader.py configure
 printf '\r\r\rclientssl\r\r\r' | coverage run --include='uploader_cli/*,CLIUploader.py' -a CLIUploader.py configure
+
 coverage report --show-missing --fail-under 100
 if [[ $CODECLIMATE_REPO_TOKEN ]] ; then
   codeclimate-test-reporter
