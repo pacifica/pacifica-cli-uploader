c11f15d3359de4512cc4e3321c0bb35d5eb4a0d0
David Brown <dmlb2000@gmail.com>
2017-09-07 10:17:43 -0700

    try getting a bit more coverage

--- travis/unit-tests.sh
+++ travis/unit-tests.sh
@@ -8,6 +8,7 @@ coverage run --include='uploader_cli/*,CLIUploader.py' -a CLIUploader.py upload 
 coverage run --include='uploader_cli/*,CLIUploader.py' -a CLIUploader.py upload --logon 10 --instrument 54 --proposal 1234a --user-of-record 11 file
 coverage run --include='uploader_cli/*,CLIUploader.py' -a CLIUploader.py configure --help
 yes | coverage run --include='uploader_cli/*,CLIUploader.py' -a CLIUploader.py configure
+printf '\r\r\rclientssl\r\r\r' | coverage run --include='uploader_cli/*,CLIUploader.py' -a CLIUploader.py configure
 coverage report --show-missing --fail-under 100
 if [[ $CODECLIMATE_REPO_TOKEN ]] ; then
   codeclimate-test-reporter
