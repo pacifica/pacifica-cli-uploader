63c617a994baff3e0b63c86ce5c17bb851fecefa
David Brown <dmlb2000@gmail.com>
2017-09-15 12:11:52 -0700

    fix pylint

--- uploader_cli/upload.py
+++ uploader_cli/upload.py
@@ -2,7 +2,7 @@
 """The upload module used to send the data to ingest."""
 
 
-def upload_main(args, md_update):
+def upload_main(args, _md_update):
     """Main upload method."""
     if args.dry_run:
         return
