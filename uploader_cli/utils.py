5fa24a2835253d23ad83376ed8814d15802ed7e5
David Brown <dmlb2000@gmail.com>
2017-09-15 23:19:42 -0700

    try getting some more debugging

--- uploader_cli/utils.py
+++ uploader_cli/utils.py
@@ -6,14 +6,16 @@ from os import makedirs, pathsep
 from os.path import expanduser, join, isdir, isfile
 
 
+logging.basicConfig()
 LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel('DEBUG')
 
 
 def system_config_path(config_file):
     """Return the system configuration path."""
     for config_dir_path in [join('{}etc'.format(pathsep), 'pacifica-cli'), join(sys.prefix, 'pacifica-cli')]:
         config_path = join(config_dir_path, config_file)
-        LOGGER.info('Trying system config %s', config_path)
+        LOGGER.debug('Trying system config %s', config_path)
         if isfile(config_path):
             return config_path
     return config_file
