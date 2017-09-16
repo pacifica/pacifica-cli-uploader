5ff690bf1972146b214bedeb3eb725aee318208e
David Brown <dmlb2000@gmail.com>
2017-09-16 00:07:20 -0700

    try setting this up better

--- travis/unit-tests.sh
+++ travis/unit-tests.sh
@@ -32,7 +32,8 @@ python CLIUploader.py configure
 # Query commands
 ############################
 coverage run --include='uploader_cli/*,CLIUploader.py' -a CLIUploader.py upload --dry-run --instrument 54 --logon dmlb2001
-printf 'q\nq\nq\n' | coverage run --include='uploader_cli/*,CLIUploader.py' -a CLIUploader.py upload --dry-run --interactive --logon dmlb2001
+export PAGER=cat
+printf '\n\n\n' | coverage run --include='uploader_cli/*,CLIUploader.py' -a CLIUploader.py upload --dry-run --interactive --logon dmlb2001
 curl -X POST -H 'content-type: application/json' 'localhost:8121/users?_id=11' -d'{ "network_id": "'`whoami`'"}'
 # this will fail...
 coverage run --include='uploader_cli/*,CLIUploader.py' -a CLIUploader.py --verbose debug upload --dry-run --instrument 54 || true
